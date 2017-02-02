a0a16be6c843c4adbcc273e9d44cee9a584e6892
ltrudeau <ltrudeau@mozilla.com>
2017-02-02 12:52:56 -0500

    Added params for --cfl and --pvq
    Also fix the makefile bug

--- configure.sh
+++ configure.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 set -e
 
-if [ -f ../makefile ]; then
-  make distclean
-fi
-
 # taken from https://github.com/tdaede/awcy/blob/master/build_codec.sh
 AWCY_FLAGS="--enable-av1 --disable-unit-tests --disable-docs"
 
-FLAGS="--enable-experimental --enable-pvq --enable-dct_only --enable-cfl --enable-debug"
+FLAGS="--enable-debug --enable-experimental"
+
+CFL=0
+PVQ=0
+while getopts "::c --long cfl::p --long pvq" opt; do
+  case $opt in
+    c)
+      CFL=1
+      PVQ=1
+      ;;
+    p)
+      PVQ=1
+      ;;
+  esac
+done
+
+
+if [ $CFL == 1 ]; then
+  FLAGS=$FLAGS" --enable-cfl"
+fi
+if [ $PVQ == 1 ]; then
+  FLAGS=$FLAGS" --enable-pvq"
+fi
+
+if [ -f ./Makefile ]; then
+  make clean
+  make distclean
+  clear
+  echo clean + distclean
+fi
+
+echo ========================================
+echo "AWCY_FLAGS="$AWCY_FLAGS
+echo "FLAGS="$FLAGS
+echo ========================================
 
 ../configure $AWCY_FLAGS $FLAGS
 
