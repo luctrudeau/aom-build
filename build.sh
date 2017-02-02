6a79b0398a03b869d30a9da581be13f8870bf939
ltrudeau <ltrudeau@mozilla.com>
2017-02-02 13:36:36 -0500

    Mostly commented it out stuff
    Later on I will add flags for gdb and valgrind

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -e
 VIDEO_PATH=~/Videos/subset1-y4m/
 
 SEQ=tiger.y4m
+#SEQ=Eaglefairy_hst_big.y4m
+#SEQ=Lufthansa_Aviation_Center_after_sunset_-_Frankfurt_-_Germany_-_near_Airport_Frankfurt_-_Fraport_-_03.y4m
 QP=55
+#QP=32
 
 # taken from https://github.com/tdaede/rd_tool/blob/master/metrics_gather.sh
 AWCY_FLAGS="--codec=av1 --frame-parallel=0 --tile-columns=0 --auto-alt-ref=2 --cpu-used=0 --passes=2 --threads=1 --kf-min-dist=1000 --kf-max-dist=1000 --lag-in-frames=25 --end-usage=q --cq-level=$QP"
@@ -13,7 +16,9 @@ FLAGS="--test-decode=fatal"
 
 make
 
-./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH$SEQ
+#valgrind --leak-check=full --show-leak-kinds=all ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH$SEQ
+#gdb -ex run --args ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH$SEQ
+./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH/$SEQ
 ./aomdec -o out.y4m out.webm
 
 # Compute PSNR
