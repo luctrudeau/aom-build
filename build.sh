585fee1305df37f002d5883f81d7e842874ed79b
Luc Trudeau <ltrudeau@mozilla.com>
2017-02-23 13:54:00 -0500

    Add better argument support and sanatizer flags

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-VIDEO_PATH=~/Videos/subset1-y4m/
+VIDEO_PATH=~/Videos/subset1-y4m
+#VIDEO_PATH=~/Videos/
+
 
 SEQ=tiger.y4m
+#SEQ=eye-bw.y4m
+#SEQ=owl.y4m
+#SEQ=hamilton.y4m
 #SEQ=Eaglefairy_hst_big.y4m
 #SEQ=Lufthansa_Aviation_Center_after_sunset_-_Frankfurt_-_Germany_-_near_Airport_Frankfurt_-_Fraport_-_03.y4m
 QP=55
@@ -14,12 +19,18 @@ AWCY_FLAGS="--codec=av1 --frame-parallel=0 --tile-columns=0 --auto-alt-ref=2 --c
 
 FLAGS="--test-decode=fatal"
 
-make
+make -j5
+
+VALGRIND="" #"valgrind " #--leak-check=full --show-leak-kinds=all"
+PERF="" #"perf stat -r 5 "
 
 #valgrind --leak-check=full --show-leak-kinds=all ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH$SEQ
 #gdb -ex run --args ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH$SEQ
-./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH/$SEQ
-./aomdec -o out.y4m out.webm
+git log --pretty=oneline -n 1
+CMD="$PERF $VALGRIND ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $VIDEO_PATH/$SEQ"
+echo $CMD 
+`$CMD`
+#./aomdec -o out.y4m out.webm
 
 # Compute PSNR
 ../../daala/tools/dump_psnr out.y4m $VIDEO_PATH$SEQ
--- configure.sh
+++ configure.sh
@@ -4,41 +4,63 @@ set -e
 # taken from https://github.com/tdaede/awcy/blob/master/build_codec.sh
 AWCY_FLAGS="--enable-av1 --disable-unit-tests --disable-docs"
 
+# Fail on asserts
 FLAGS="--enable-debug --enable-experimental"
+
+# Sanitizer Flags
+SAN_CFLAGS="-fsanitize=address,undefined -Wformat -Werror=format-security -Werror=array-bounds -g"
+SAN_LDFLAGS="-fsanitize=address,undefined"
 
 CFL=0
 PVQ=0
-DAALA_DCT=0
-LIMIT=0
-while getopts ":c --long cfl:p --long pvq:d --long daaladct:l --long limit_4x4:" opt; do
-  case $opt in
-    c)
-      CFL=1
-      PVQ=1
-      ;;
-    p)
-      PVQ=1
-      ;;
-    d)
-      DAALA_DCT=1
-      ;;
-    l)
-      LIMIT=1
+DAALA_TX=0
+LIMIT_TX_4X4=0
+LIMIT_TX_8X8=0
+DCT_ONLY=0
+USE_C_TX=0
+UV_DC_PRED_ONLY=0
+SANITIZE=0
+
+# Transform long options to short ones
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--cfl") CFL=1 ;;
+    "--pvq") PVQ=1 ;;
+    "--daala_tx") DAALA_TX=1 ;;
+    "--limit_tx_4x4") LIMIT_TX_4X4=1 ;;
+    "--limit_tx_8x8") LIMIT_TX_8X8=1 ;;
+    "--dct_only") DCT_ONLY=1 ;;
+    "--use_c_tx") USE_C_TX=1 ;;
+    "--uv_dc_pred_only") UV_DC_PRED_ONLY=1 ;;
+    "--sanitize") SANITIZE=1 ;;
+    *)        set -- "$@" "$arg"
   esac
 done
 
-
 if [ $CFL == 1 ]; then
-  FLAGS=$FLAGS" --enable-cfl"
+  FLAGS=$FLAGS" --enable-pvq_cfl"
 fi
 if [ $PVQ == 1 ]; then
   FLAGS=$FLAGS" --enable-pvq"
 fi
-if [ $DAALA_DCT == 1 ]; then
-  FLAGS=$FLAGS" --enable-daala_dct"
+if [ $DAALA_TX == 1 ]; then
+  FLAGS=$FLAGS" --enable-daala_tx"
 fi
-if [ $LIMIT == 1 ]; then
+if [ $LIMIT_TX_4X4 == 1 ]; then
   FLAGS=$FLAGS" --enable-limit_4x4"
+fi
+if [ $LIMIT_TX_8X8 == 1 ]; then
+  FLAGS=$FLAGS" --enable-limit_8x8"
+fi
+if [ $DCT_ONLY == 1 ]; then
+  FLAGS=$FLAGS" --enable-dct_only"
+fi
+if [ $USE_C_TX == 1 ]; then
+  FLAGS=$FLAGS" --enable-use_c_tx"
+fi
+if [ $UV_DC_PRED_ONLY == 1 ]; then
+  FLAGS=$FLAGS" --enable-uv_dc_pred_only"
 fi
 
 if [ -f ./Makefile ]; then
@@ -48,10 +70,18 @@ if [ -f ./Makefile ]; then
   echo clean + distclean
 fi
 
-echo ========================================
-echo "AWCY_FLAGS="$AWCY_FLAGS
-echo "FLAGS="$FLAGS
-echo ========================================
+CMD=''
+if [ $SANITIZE == 1 ]; then
+  echo ======================================== 
+  echo               SANITIZER BUILD
+  echo CFLAGS="$SAN_CFLAGS"
+  echo LDFLAGS="$SAN_LDFLAGS"
+  echo ======================================== 
+  CMD=$CMD'CFLAGS="$SAN_CFLAGS" LDFLAGS="$SAN_LDFLAGS"'
+fi
 
-../configure $AWCY_FLAGS $FLAGS
+CMD=$CMD" ../configure $AWCY_FLAGS $FLAGS"
 
+git log --pretty=oneline -n 1
+echo $CMD
+eval $CMD
