5ba357ac445749fa666c46df762e91f41c59f0b2
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-17 14:30:42 -0400

    Minor updates

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-SEQ=~/Videos/hamilton.y4m
+#SEQ=~/Videos/hamilton.y4m
+#SEQ=~/Videos/PerfectMeerkat.y4m
+#SEQ=~/Videos/subset1-y4m/Wasserfassstelle_von_1898_im_Schanerloch.y4m
+SEQ=~/Videos/subset1-y4m/Fruits_oranges,_jardin_japonais_2.y4m
 
 TIGER=0
 OWL=0
 VALGRIND=0
 PERF=0
+GDB=0
 
 for arg in "$@"; do
   shift
@@ -15,6 +19,7 @@ for arg in "$@"; do
     "--owl") OWL=1 ;;
     "--valgrind") VALGRIND=1 ;;
     "--perf") PERF=1 ;;
+    "--gdb") GDB=1 ;;
     *)        set -- "$@" "$arg"
   esac
 done
@@ -28,27 +33,32 @@ if [ $OWL == 1 ]; then
 fi
 VALGRIND_CMD=""
 if [ $VALGRIND == 1 ]; then
-  VALGRIND_CMD="valgrind " #--leak-check=full --show-leak-kinds=all"
+  VALGRIND_CMD="valgrind --vgdb-error=1 " #--leak-check=full --show-leak-kinds=all --track-origins=yes "
 fi
 PERF_CMD=""
 if [ $PERF == 1 ]; then
   PERF_CMD="perf stat -r 5 "
 fi
+GDB_CMD=""
+if [ $GDB == 1 ]; then
+  GDB_CMD="gdb -ex=r --args "
+fi
 
 QP=55
+#QP=43
 
 # taken from https://github.com/tdaede/rd_tool/blob/master/metrics_gather.sh
 AWCY_FLAGS="--codec=av1 --frame-parallel=0 --tile-columns=0 --auto-alt-ref=2 --cpu-used=0 --passes=2 --threads=1 --kf-min-dist=1000 --kf-max-dist=1000 --lag-in-frames=25 --end-usage=q --cq-level=$QP"
 
-FLAGS="--test-decode=fatal"
+FLAGS="-v --test-decode=fatal"
 
 make -j5
 
 git log --pretty=oneline -n 1
-CMD="$PERF_CMD $VALGRIND_CMD ./aomenc $AWCY_FLAGS $FLAGS -o out.webm $SEQ"
+CMD="$GDB_CMD $PERF_CMD $VALGRIND_CMD ./aomenc $AWCY_FLAGS $FLAGS --ivf -o out.ivf $SEQ"
 echo $CMD
-`$CMD`
-./aomdec -o out.y4m out.webm
+eval $CMD
+./aomdec -o out.y4m out.ivf
 
 # Compute PSNR
 ../../daala/tools/dump_psnr out.y4m $SEQ
--- configure.sh
+++ configure.sh
@@ -2,16 +2,17 @@
 set -e
 
 # taken from https://github.com/tdaede/awcy/blob/master/build_codec.sh
-AWCY_FLAGS="--enable-av1 --disable-unit-tests --disable-docs"
+AWCY_FLAGS="--enable-av1 --disable-unit-tests --disable-docs" # --enable-inspection --enable-accounting"
 
 # Fail on asserts
-FLAGS="--enable-debug --enable-experimental"
+FLAGS="--enable-debug --enable-experimental "
 
 # Sanitizer Flags
 SAN_CFLAGS="-fsanitize=address,undefined -Wformat -Werror=format-security -Werror=array-bounds -g"
 SAN_LDFLAGS="-fsanitize=address,undefined"
 
 CFL=0
+PVQ_CFL=0
 PVQ=0
 DAALA_TX=0
 LIMIT_TX_4X4=0
@@ -20,10 +21,12 @@ DCT_ONLY=0
 USE_C_TX=0
 UV_DC_PRED_ONLY=0
 SANITIZE=0
+ANALYZER=0
 
 for arg in "$@"; do
   shift
   case "$arg" in
+    "--pvq_cfl") PVQ_CFL=1 ;;
     "--cfl") CFL=1 ;;
     "--pvq") PVQ=1 ;;
     "--daala_tx") DAALA_TX=1 ;;
@@ -33,11 +36,15 @@ for arg in "$@"; do
     "--use_c_tx") USE_C_TX=1 ;;
     "--uv_dc_pred_only") UV_DC_PRED_ONLY=1 ;;
     "--sanitize") SANITIZE=1 ;;
+    "--analyzer") ANALYZER=1 ;;
     *)        set -- "$@" "$arg"
   esac
 done
 
 if [ $CFL == 1 ]; then
+  FLAGS=$FLAGS" --enable-cfl"
+fi
+if [ $PVQ_CFL == 1 ]; then
   FLAGS=$FLAGS" --enable-pvq_cfl"
 fi
 if [ $PVQ == 1 ]; then
@@ -61,6 +68,9 @@ fi
 if [ $UV_DC_PRED_ONLY == 1 ]; then
   FLAGS=$FLAGS" --enable-uv_dc_pred_only"
 fi
+if [ $ANALYZER == 1 ]; then
+  FLAGS=$FLAGS" --enable-analyzer"
+fi
 
 if [ -f ./Makefile ]; then
   make clean
@@ -79,7 +89,7 @@ if [ $SANITIZE == 1 ]; then
   CMD=$CMD'CFLAGS="$SAN_CFLAGS" LDFLAGS="$SAN_LDFLAGS"'
 fi
 
-CMD=$CMD" ../configure $AWCY_FLAGS $FLAGS"
+CMD=$CMD" CFLAGS='-Wall -Wextra -Werror ' ../configure $AWCY_FLAGS $FLAGS"
 
 git log --pretty=oneline -n 1
 echo $CMD
