6a2c20323d63bacb510c562e0749a4b270ab4791
Luc Trudeau <luc@trud.ca>
2017-06-02 16:18:32 -0400

    Update scripts

--- build.sh
+++ build.sh
@@ -2,12 +2,25 @@
 set -e
 
 #SEQ=~/Videos/hamilton.y4m
+SEQ=~/Videos/testsrc-1frame.y4m
+#SEQ=~/Videos/meerkat_444.y4m
+#SEQ=~/Videos/ducks_take_off_444_720p50_frame1.y4m
 #SEQ=~/Videos/PerfectMeerkat.y4m
-#SEQ=~/Videos/subset1-y4m/Wasserfassstelle_von_1898_im_Schanerloch.y4m
-SEQ=~/Videos/subset1-y4m/Fruits_oranges,_jardin_japonais_2.y4m
+#SEQ=~/Videos/subset1-y4m/205_-_Vallée_de_Colca_-_Panorama_-_Juin_2010_-_5_de_6.y4m
+#SEQ="/home/ltrudeau/Videos/subset1-y4m/Catedral_de_Toledo.Altar_Mayor_(huge).y4m"
+#eqz:EQ=~/Videos/subset1-y4m/Wasserfassstelle_von_1898_im_Schanerloch.y4m
+#SEQ=~/Videos/subset1-y4m/205_-_Vallée_de_Colca_-_Panorama_-_Juin_2010_-_5_de_6.y4m
+#SEQ=~/Videos/subset1-y4m/IENA_-_Avenches_-_6.y4m
+#SEQ=~/Videos/subset1-y4m/Washington_Monument,_Washington,_D.C._04037u_original.y4m
+#SEQ=~/Videos/subset1-y4m/US_Navy_111117-N-UB993-082_A_Sailor_examines_a_patient_during_drill.y4m
+#SEQ=~/Videos/akiyo_cif_short.y4m
+#SEQ=~/Videos/Netflix_Aerial_4096x2160_60fps_10bit_420-1frame.y4m
+#SEQ=~/Videos/football_422_ntsc.y4m
 
 TIGER=0
 OWL=0
+PONT=0
+FRUIT=0
 VALGRIND=0
 PERF=0
 GDB=0
@@ -17,6 +30,8 @@ for arg in "$@"; do
   case "$arg" in
     "--tiger") TIGER=1 ;;
     "--owl") OWL=1 ;;
+    "--pont") PONT=1 ;;
+    "--fruit") FRUIT=1 ;;
     "--valgrind") VALGRIND=1 ;;
     "--perf") PERF=1 ;;
     "--gdb") GDB=1 ;;
@@ -31,9 +46,15 @@ fi
 if [ $OWL == 1 ]; then
   SEQ=~/Videos/owl.y4m
 fi
+if [ $PONT == 1 ]; then
+  SEQ=~/Videos/subset1-y4m/125_-_Québec_-_Pont_de_Québec_de_nuit_-_Septembre_2009.y4m
+fi
+if [ $FRUIT == 1 ]; then
+  SEQ=~/Videos/subset1-y4m/Fruits_oranges,_jardin_japonais_2.y4m
+fi
 VALGRIND_CMD=""
 if [ $VALGRIND == 1 ]; then
-  VALGRIND_CMD="valgrind --vgdb-error=1 " #--leak-check=full --show-leak-kinds=all --track-origins=yes "
+  VALGRIND_CMD="valgrind --leak-check=full --vgdb-error=1 --show-leak-kinds=all"  # --track-origins=yes "
 fi
 PERF_CMD=""
 if [ $PERF == 1 ]; then
@@ -44,11 +65,14 @@ if [ $GDB == 1 ]; then
   GDB_CMD="gdb -ex=r --args "
 fi
 
+#QP=63
 QP=55
 #QP=43
+#QP=32
+#QP=20
 
 # taken from https://github.com/tdaede/rd_tool/blob/master/metrics_gather.sh
-AWCY_FLAGS="--codec=av1 --frame-parallel=0 --tile-columns=0 --auto-alt-ref=2 --cpu-used=0 --passes=2 --threads=1 --kf-min-dist=1000 --kf-max-dist=1000 --lag-in-frames=25 --end-usage=q --cq-level=$QP"
+AWCY_FLAGS="--codec=av1 --frame-parallel=0 --tile-columns=0 --auto-alt-ref=2 --cpu-used=0 --passes=2 --threads=1 --kf-min-dist=1000 --kf-max-dist=1000 --lag-in-frames=25 --end-usage=q --cq-level=$QP --limit=1"
 
 FLAGS="-v --test-decode=fatal"
 
@@ -57,12 +81,20 @@ make -j5
 git log --pretty=oneline -n 1
 CMD="$GDB_CMD $PERF_CMD $VALGRIND_CMD ./aomenc $AWCY_FLAGS $FLAGS --ivf -o out.ivf $SEQ"
 echo $CMD
-eval $CMD
+#eval "$CMD"
+$GDB_CMD $PERF_CMD $VALGRIND_CMD ./aomenc $AWCY_FLAGS $FLAGS --ivf -o out.ivf $SEQ
 ./aomdec -o out.y4m out.ivf
 
-# Compute PSNR
+# compute psnr
 ../../daala/tools/dump_psnr out.y4m $SEQ
+
+# compute CIEDE2000
+#echo CIEDE2000
+#../../daala/tools/dump_ciede2000.py out.y4m $SEQ
+
 
 # Create a PNG and View it
 ../../daala/tools/y4m2png -o out.png out.y4m
 eog out.png
+#mpv --loop --pause out.y4m
+#examples/analyzer out.ivf
--- configure.sh
+++ configure.sh
@@ -2,10 +2,10 @@
 set -e
 
 # taken from https://github.com/tdaede/awcy/blob/master/build_codec.sh
-AWCY_FLAGS="--enable-av1 --disable-unit-tests --disable-docs" # --enable-inspection --enable-accounting"
+AWCY_FLAGS=" --enable-av1 --disable-unit-tests --disable-docs --enable-inspection --enable-accounting --enable-analyzer"
 
 # Fail on asserts
-FLAGS="--enable-debug --enable-experimental "
+FLAGS="--enable-debug --enable-experimental --disable-cdef --enable-aom_highbitdepth"
 
 # Sanitizer Flags
 SAN_CFLAGS="-fsanitize=address,undefined -Wformat -Werror=format-security -Werror=array-bounds -g"
@@ -17,26 +17,34 @@ PVQ=0
 DAALA_TX=0
 LIMIT_TX_4X4=0
 LIMIT_TX_8X8=0
-DCT_ONLY=0
+HBD=0
 USE_C_TX=0
 UV_DC_PRED_ONLY=0
 SANITIZE=0
 ANALYZER=0
+EC_ADAPT=0
+CHROMA_2X2=0
+CHROMA_SUB8X8=0
+EC_SMALLMUL=0
 
 for arg in "$@"; do
   shift
   case "$arg" in
     "--pvq_cfl") PVQ_CFL=1 ;;
     "--cfl") CFL=1 ;;
+    "--ec_adapt") EC_ADAPT=1 ;;
+    "--chroma_2x2") CHROMA_2X2=1 ;;
+    "--chroma_sub8x8") CHROMA_SUB8X8=1 ;;
     "--pvq") PVQ=1 ;;
     "--daala_tx") DAALA_TX=1 ;;
     "--limit_tx_4x4") LIMIT_TX_4X4=1 ;;
     "--limit_tx_8x8") LIMIT_TX_8X8=1 ;;
-    "--dct_only") DCT_ONLY=1 ;;
+    "--hbd") HBD=1 ;;
     "--use_c_tx") USE_C_TX=1 ;;
     "--uv_dc_pred_only") UV_DC_PRED_ONLY=1 ;;
     "--sanitize") SANITIZE=1 ;;
     "--analyzer") ANALYZER=1 ;;
+    "--smallmul") EC_SMALLMUL=1 ;;
     *)        set -- "$@" "$arg"
   esac
 done
@@ -59,8 +67,8 @@ fi
 if [ $LIMIT_TX_8X8 == 1 ]; then
   FLAGS=$FLAGS" --enable-limit_8x8"
 fi
-if [ $DCT_ONLY == 1 ]; then
-  FLAGS=$FLAGS" --enable-dct_only"
+if [ $HBD == 1 ]; then
+  FLAGS=$FLAGS" --enable-highbitdepth"
 fi
 if [ $USE_C_TX == 1 ]; then
   FLAGS=$FLAGS" --enable-use_c_tx"
@@ -70,6 +78,18 @@ if [ $UV_DC_PRED_ONLY == 1 ]; then
 fi
 if [ $ANALYZER == 1 ]; then
   FLAGS=$FLAGS" --enable-analyzer"
+fi
+if [ $EC_ADAPT == 1 ]; then
+  FLAGS=$FLAGS" --enable-ec_adapt"
+fi
+if [ $CHROMA_2X2 == 1 ]; then
+  FLAGS=$FLAGS" --enable-chroma_2x2"
+fi
+if [ $CHROMA_SUB8X8 == 1 ]; then
+  FLAGS=$FLAGS" --enable-chroma_sub8x8"
+fi
+if [ $EC_SMALLMUL == 1 ]; then
+  FLAGS=$FLAGS" --enable-ec_smallmul"
 fi
 
 if [ -f ./Makefile ]; then
