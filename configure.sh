bf50e0b939782eade17159ecfeba8f3b5d4bfd77
Luc Trudeau <luc@trud.ca>
2018-01-22 11:19:55 -0500

    Fix Sanitizer typo

--- configure.sh
+++ configure.sh
@@ -19,10 +19,10 @@ done
 
 SANITIZER=""
 if [ $TSAN == 1 ]; then
-  $SANITIZER="-DSANITIZE=thread"
+  SANITIZER="-DSANITIZE=thread"
 fi
 if [ $ASAN == 1 ]; then
-  $SANITIZER="-DSANITIZE=address"
+  SANITIZER="-DSANITIZE=address"
 fi
 
 COMPILER="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
@@ -32,7 +32,7 @@ CCACHE="-DENABLE_CCACHE=1"
 # https://github.com/tdaede/awcy/blob/5e8e5324e4835445f1a42c2bfc39e1cb36cecb11/build_codec.sh#L50
 AWCY_FLAGS="-DCONFIG_UNIT_TESTS=0 -DENABLE_DOCS=0 -DCMAKE_BUILD_TYPE=Debug"
 
-PARAMS="$AWCY_FLAGS $CCACHE $COMPILER $SANITIZER"
+PARAMS="$COMPILER $AWCY_FLAGS $CCACHE $SANITIZER"
 
 git log --pretty=oneline -n 1
 set -x
